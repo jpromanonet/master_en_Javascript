ba0605758814f0114f40d6412ab6b1622b4f3b53
Juan P. Romano <programming@jpromano.net>
2021-04-28 09:44:26 -0300

    Hola mundo

--- holaMundo/js/script.js
+++ holaMundo/js/script.js
@@ -1,2 +1,8 @@
+// Con alerts
 alert("Hello World with JS!");
 alert("Welcome to the JS Master");
+
+// Con document write
+document.write("<h1>Master en Javascript</h1>");
+document.write("<p>El primer script en JS(It's a lie!)</p>");
+document.write("Hola mundo desde un archivo externo");
